2688607e385270d78f962dc6f6b351b8d948ceab
Angie Shin <96707332+Angie629@users.noreply.github.com>
2024-03-22 21:04:33 -0600

    Update CleanPopulationDataset.py

--- CleanPopulationDataset.py
+++ CleanPopulationDataset.py
@@ -11,5 +11,5 @@ cleaned_data = data[['country_name', 'year', 'population']]
 yearly_population_sum = cleaned_data.groupby('year')['population'].sum().reset_index()
 
 # Save the aggregated data to an Excel file
-aggregated_file_path = 'C:/Users/Angie/Downloads/aggregated_population_by_year.xlsx'
+aggregated_file_path = 'C:/Users/Angie/Downloads/cleaned_population_by_year.xlsx'
 yearly_population_sum.to_excel(aggregated_file_path, index=False)
